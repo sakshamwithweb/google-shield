c37a248bca34c71c3022c916640c902705ac8dcc
sakshamwithweb <websaksham39@gmail.com>
2024-11-08 20:23:12 +0530

    changedchat model

--- app/api/get/sendRequestToAI/route.js
+++ app/api/get/sendRequestToAI/route.js
@@ -8,8 +8,8 @@ const openai = new OpenAI({
 export async function POST(request) {
     const {speechToText, time, weather, motion, safePlaces} = await request.json();
     const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: `Hey Chatgpt, I am a women and right now it is ${time} and the weather is ${weather}. I am ${motion} and safe places near me is ${safePlaces}. I have a query - ${speechToText}. I want you to give me possible and best actions as an array Please`}],
+        model: "chatgpt-4o-latest",
+        messages: [{ role: "user", content: `Hey Chatgpt, I am a women and right now it is ${time} and the weather is ${weather}. I am ${motion} and safe places near me is ${safePlaces}. I have a query - ${speechToText}. I want you to give me possible and best actions as an array Please\nDon't use md anywhere.`}],
       });
       console.log( response.choices[0].message.content)
       return NextResponse.json({response: response.choices[0].message.content})
