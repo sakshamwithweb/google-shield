6bc74c71eeb319ebec1633c9c82e5909fc633d29
sakshamwithweb <websaksham39@gmail.com>
2024-11-08 20:43:02 +0530

    1st try to make the prompt of chat ai better

--- app/api/get/sendRequestToAI/route.js
+++ app/api/get/sendRequestToAI/route.js
@@ -9,7 +9,7 @@ export async function POST(request) {
     const {speechToText, time, weather, motion, safePlaces} = await request.json();
     const response = await openai.chat.completions.create({
         model: "chatgpt-4o-latest",
-        messages: [{ role: "user", content: `Hey Chatgpt, I am a women and right now it is ${time} and the weather is ${weather}. I am ${motion} and safe places near me is ${safePlaces}. I have a query - ${speechToText}. I want you to give me possible and best actions as an array Please\nDon't use md anywhere.`}],
+        messages: [{ role: "user", content: `Hey Chatgpt, I am a women and right now it is ${time} and the weather is ${weather}. I am ${motion} and safe places near me is ${safePlaces}. I have a query - ${speechToText}. I want you to give me possible and best actions as an array Please\nDon't use md anywhere. Use my data and tell actions which I could do . Please say the actions in very simple and short way so as to I could understand even I am fraustrated `}],
       });
       console.log( response.choices[0].message.content)
       return NextResponse.json({response: response.choices[0].message.content})
